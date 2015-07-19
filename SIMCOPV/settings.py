a41e09a20ba53e906b44b6c70f2bdeae6436fa46
Jorge Torres <jv_torres64@rocketmail.com>
2015-07-19 11:48:39 -0500

    setting oracle database

--- SIMCOPV/settings.py
+++ SIMCOPV/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'SIMCOPV.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'DBPrueba',
+        'USER': 'hr',
+        'PASSWORD': 'Admin20081994',
+        'HOST': '192.168.56.102',
+        'PORT': '1521',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
