c8a6d4952a25a58f4856012ff0e2eb1e3f16d5e6
jorgevtorresr <jorgevtorresr@gmail.com>
2015-12-02 18:04:21 -0500

    Fixed server validation permisos

--- apps/permisos/views.py
+++ apps/permisos/views.py
@@ -1,8 +1,11 @@
 #! python2
 # -*- coding: utf-8 -*-
+from apps.usuarios.models import Periodo
 from datetime import datetime
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
+from django.db.models import Max
 from django.http import HttpResponseRedirect
 from django.shortcuts import render 
 from rest_framework import viewsets
@@ -19,24 +22,40 @@ def permiso(request):
 		hora_fin = request.POST.get('hora_fin','')
 		descripcion = request.POST.get('descripcion','')
 		# I have to obligatorily make server validation		
-		date_inicio = datetime.strptime(fecha_inicio,'%Y-%m-%d')
-		date_fin = datetime.strptime(fecha_fin,'%Y-%m-%d')
+		date_inicio = datetime.strptime(fecha_inicio+hora_inicio,'%Y-%m-%d%H:%M')
+		date_fin = datetime.strptime(fecha_fin+hora_fin,'%Y-%m-%d%H:%M')
 		# Take the next year to compare date and control the overflow
-		date_after = datetime.strptime("{0}-01-01".format(datetime.now().year),'%Y-%m-%d')
+		date_after = datetime.strptime("{0}-01-01".format(datetime.now().year+1),'%Y-%m-%d')
 		if date_fin < date_inicio:
-			pass
-		elif fecha_inicio == fecha_fin:
-			pass
-		elif date_inicio < datetime.now() || date_inicio < datetime.now():
-			pass
-		elif date_inicio > date_after || date_fin > date_after:
-			pass
-		elif fecha_inicio == "" || fecha_fin == "":
-			pass
+			messages.error(request,"Las fechas no son consistentes")
+		elif fecha_inicio == fecha_fin and date_inicio.time() > date_fin.time():
+			messages.error(request,"Las horas no son consistentes")
+		elif date_inicio < datetime.now() or date_fin < datetime.now():
+			messages.error(request,"Las fechas no pueden ser menores al día en curso")
+		elif date_inicio > date_after or date_fin > date_after:
+			messages.error(request,"Las fechas no deben sobrepasar el año en curso")
+		elif fecha_inicio == "" or fecha_fin == "":
+			messages.error(request,"Ingrese la fecha de inicio y la de fin")
 		elif hora_inicio == "" or hora_fin == "" or descripcion == "":
-			pass
+			messages.error(request,"Ingrese los datos completos")
 		else:
-			pass # Here comes the truth
+			# Here comes the truth
+			periodos = Periodo.objects.filter(usuario=request.user)
+			max = periodos.aggregate(max=Max('anio_periodo'))
+			periodomax = []
+			if periodos == []:
+				messages.error(request,"Usted no tiene permisos asignados.. Por favor contacte con el departamento de RRHH")
+			else:
+				try:
+					periodomax = periodos.get(anio_periodo=max["max"])
+				except:
+					pass
+				if periodomax == []:
+					pass
+				else:
+					permiso = Permiso(fecha_inicio=date_inicio,fecha_fin=date_fin,descripcion=descripcion,usuario=request.user,periodo=periodomax)
+					#permiso.save()
+					messages.success(request,"Permiso ingresado correctamente")
 		return HttpResponseRedirect(reverse("permiso"))	
 	return render(request, "permisos/permiso.html",{})
 
